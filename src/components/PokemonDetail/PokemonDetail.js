77a0fdea2cbac802a37df12031f1131127317fd5
Agnieszka26 <76781871+Agnieszka26@users.noreply.github.com>
2022-01-11 15:27:01 +0100

    link

--- src/components/PokemonDetail/PokemonDetail.js
+++ src/components/PokemonDetail/PokemonDetail.js
@@ -2,11 +2,9 @@ import React from "react";
 // import {
 // } from "./Card.styles";
 const PokemonDetail = (props) => {
-  return (
-    <div >
-    name: props.name
-    </div>
-  );
+  return <div>name: props.name</div>;
 };
 
 export default PokemonDetail;
+
+// https://stackoverflow.com/questions/31079081/programmatically-navigate-using-react-router
